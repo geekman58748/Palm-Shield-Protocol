e7cd8ed9f169696105960e551f6cb5fb7c3eb414
Maxx.eth <88834630+geekman58748@users.noreply.github.com>
2026-05-01 01:10:08 +0100

    Create State.js

---
+++ palmshield/JS/State.js
@@ -0,0 +1,14 @@
+const state = {
+  wallet:         null,
+  walletShort:    null,
+  role:           null,
+  submissions:    [],
+  myVotes:        {},
+  daoVotesCast:   0,
+  hunterBalance:  0,
+  selectedThreat: null,
+  bountyAmt:      250,
+  uploadedFile:   null,
+  uploadedImage:  null,
+  activeModal:    null,
+};
